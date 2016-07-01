13ccc6df5cb6e0f6520d97ccc80e594bbffcf9a9
Dominique Quatravaux <dominique@quatravaux.org>
2016-07-01 14:15:57 +0200

    Récupération des fiches (WIP)

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -5,33 +5,43 @@ var Trello = require("trello");
 var trello = new Trello(secrets["user_key"], secrets["trello_readonly_key"]);
 var userId = secrets["user_id"];
 
+function mkdirtree(/* elements */) {
+    var elements = Array.prototype.slice.call(arguments);
+    try {
+        fs.mkdirSync('./boards/' + elements.join("/"));
+    } catch(e) {
+        if(e.code !== 'EEXIST'){
+            throw e;
+        }
+    }
+}
+
+function makeBoardDir(board) {
+    var coolName = board.url.split("/");
+    mkdirtree(coolName[coolName.length-1]);
+}
+
+function makeListDir(board, list) {
+    var coolName = board.url.split("/");  // XX REPEATED
+    var listName = list.name;
+    mkdirtree(coolName[coolName.length-1], listName);
+}
+function handleError(err) {
+    console.error(err);
+}
 trello.getBoards(userId, function(err, value) {
 
     value.map(function (board) {
-        var coolName = board.url.split("/");
-        try{
-            fs.mkdirSync('./boards/'+ coolName[coolName.length-1]);
-        } catch(e){
-            if(e.code !== 'EEXIST'){
-                throw e;
-            }
-        }
+        makeBoardDir(board);
         trello.getListsOnBoard(board.id, function (err, value) {
             value.map(function (list) {
-                try{
-                    fs.mkdirSync('./boards/'+ coolName[coolName.length-1] + "/" + list.name);
-                } catch(e){
-                    if(e.code !== 'EEXIST'){
-                        throw e;
-                    }
-                }
-                console.error(err);
-                console.log(value);
+                makeListDir(board, list);
+                handleError(err);
+                trello.getCardsOnList(list.id, function (err, value) {
+                    console.error(err);
+                    console.log(value);
+                });
             });
-        });
-        trello.getCardsOnBoard(board.id, function (err, value) {
-            console.error(err);
-            console.log(value);
         });
     });
     console.error(err);
